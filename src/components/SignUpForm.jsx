74c3d2392834511cdc3511d8600a9251ce23e88f
Saurabh Bhise <saurabhbhise16@live.com>
2023-06-11 20:05:00 +0000

    Updated SignUpForm.jsx with help of useField render-prop and eleminating prop drilling

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { Formik, Field, Form, ErrorMessage, useField } from "formik";
+import * as Yup from "yup";
+
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.label || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched || meta.error ? <div>{meta.error}</div> : null};
+    </>
+  );
+};
+
+const MyCheckbox = ({ children, ...props }) => {
+  // React treats radios and checkbox inputs differently from other input types: select and textarea.
+  // Formik does this too! When you specify `type` to useField(), it will
+  // return the correct bag of props for you -- a `checked` prop will be included
+  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
+  const [field, meta] = useField({ ...props, type: "checkbox" });
+  return (
+    <div>
+      <label className="checkbox-input">
+        <input type="checkbox" {...field} {...props} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const MySelect = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
 
 const SignupForm = () => {
   return (
     <Formik
-      initialValues={{ firstName: '', lastName: '', email: '' }}
+      initialValues={{ firstName: "", lastName: "", email: "" }}
       validationSchema={Yup.object({
         firstName: Yup.string()
-          .max(15, 'Must be 15 characters or less')
-          .required('Required'),
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
         lastName: Yup.string()
-          .max(20, 'Must be 20 characters or less')
-          .required('Required'),
-        email: Yup.string().email('Invalid email address').required('Required'),
+          .max(20, "Must be 20 characters or less")
+          .required("Required"),
+        email: Yup.string().email("Invalid email address").required("Required"),
       })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
@@ -23,18 +66,38 @@ const SignupForm = () => {
       }}
     >
       <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" type="text" />
-        <ErrorMessage name="firstName" />
+        <MyTextInput
+          label="firstName"
+          name="firstName"
+          type="text"
+          placeholder="jane"
+        />
 
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" type="text" />
-        <ErrorMessage name="lastName" />
+        <MyTextInput
+          label="lastName"
+          name="lastName"
+          type="text"
+          placeholder="murthy"
+        />
 
-        <label htmlFor="email">Email Address</label>
-        <Field name="email" type="email" />
-        <ErrorMessage name="email" />
+        <MyTextInput
+          label="Email Address"
+          name="email"
+          type="email"
+          placeholder="jane@formik.com"
+        />
 
+        <MySelect label="Job Type" name="jobType">
+          <option value="">Select a job type</option>
+          <option value="designer">Designer</option>
+          <option value="development">Developer</option>
+          <option value="product">Product Manager</option>
+          <option value="other">Other</option>
+        </MySelect>
+
+        <MyCheckbox name="acceptedTerms">
+          I accept the terms and conditions
+        </MyCheckbox>
         <button type="submit">Submit</button>
       </Form>
     </Formik>
