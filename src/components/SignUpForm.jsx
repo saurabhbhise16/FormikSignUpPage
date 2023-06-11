10327faf3baf2623acf50b76e1bbedd60e1ace0e
Saurabh Bhise <saurabhbhise16@live.com>
2023-06-11 06:13:07 +0000

    Update 6 files

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,19 +1,71 @@
 import React from "react";
 import { useFormik } from "formik";
 
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "First Name Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Name cannot be greater than 15";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Last Name Required";
+  } else if (values.lastName.length > 15) {
+    errors.lastName = "Name cannot be greater than 15";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+  return errors;
+};
+
 const SignupForm = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
+  // Note that we have to initialize ALL of fields with values. These
+  // could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell
+  // at us.
   const formik = useFormik({
     initialValues: {
-      email: ""
+      firstName: "",
+      lastName: "",
+      email: "",
     },
+    validate,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
-    }
+    },
   });
   return (
     <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.firstName && formik.errors.firstName ? (
+        <div>{formik.errors.firstName}</div>
+      ) : null}
+      <br />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.firstName && formik.errors.lastName ? (
+        <div>{formik.errors.lastName}</div>
+      ) : null}
+      <br />
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
@@ -21,7 +73,13 @@ const SignupForm = () => {
         type="email"
         onChange={formik.handleChange}
         value={formik.values.email}
+        onBlur={formik.handleBlur}
       />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
+      <br></br>
+      <br></br>
 
       <button type="submit">Submit</button>
     </form>
